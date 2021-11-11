fbbbb453ab7cd5ad301294d5b36f7c808d63765b
Russo Salvatore <russo.salvatore@osdbsesto>
2021-11-11 11:44:10 +0100

    add ,save in progress

--- calcolatrice.js
+++ calcolatrice.js
@@ -1,17 +1,43 @@
-$("#btn").click(function(){
+$("#btn").click(()=>calcola("+")),
+$("#btn2").click(()=>calcola("-")),
+$("#btn3").click(()=>calcola("*")),
+$("#btn4").click(()=>calcola("/"))
+
+
+
+var calcola=function(segno){
     var input=$("#num1");
     var input2=$("#num2");
     console.log("input1", input.val());
     console.log("input2", input2.val());
+    console.log("segno", segno);
     if(input.val() && input2.val()){
     var n1=parseInt(input.val());
     var n2=parseInt(input2.val());
-    var risultato=n1+n2;
+    switch(segno){
+        case "+":
+            var risultato=n1+n2;
+            break;
+            case "-":
+            var risultato=n1-n2;
+            break;
+            case "*":
+            var risultato=n1*n2;
+            break;
+            case "/":
+            var risultato=n1/n2;
+            break;
+    } 
     var string=
-    "<tr><td>"+ n1 + "</td><td style='text-align:centre'>+</td><td>"+  n2+
+    "<tr><td>"+ n1 + "</td><td style='text-align:centre'>"+segno+"</td><td>"+  n2+
     "</td><td>"+risultato +"</td></tr>";
+    localStorage.setItem(string);
+   localStorage.getItem(string);
     $("#tab").append(string);
     };
     input.val("");
     input.val("");
-    });
+    } 
+
+
+    
